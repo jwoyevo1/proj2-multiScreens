ebf6a3724bbcce62ba866b8cdfd90e382f5f05b2
jwoyevo1 <33128688+jwoyevo1@users.noreply.github.com>
2021-03-22 17:43:49 -0400

    Add files via upload
    in swift

---
+++ proj2-multiScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  proj2-multiScreen
+//
+//  Created by James Woyevodsky on 3/22/21.
+//  Copyright © 2021 Binghamton University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
